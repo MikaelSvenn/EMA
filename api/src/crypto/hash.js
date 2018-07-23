0ff2d86cea249fd13a6313e729ab0ae4de796d9f
Mikael Svenn <mikael.svenn@mixu.fi>
2018-07-23 23:45:54 +0300

    Enabled hmac key to also be given as a parameter for hash

--- api/src/crypto/hash.js
+++ api/src/crypto/hash.js
@@ -1,8 +1,15 @@
 import nodeCrypto from 'crypto';
 
-export default (crypto = nodeCrypto) => (content) => {
-  const key = crypto.randomBytes(64).toString('base64');
-  const hmac = crypto.createHmac('sha256', key);
+export default (crypto = nodeCrypto) => (content, key) => {
+  let hashKey;
+
+  if (!key) {
+    hashKey = crypto.randomBytes(64).toString('base64');
+  } else {
+    hashKey = key;
+  }
+
+  const hmac = crypto.createHmac('sha256', hashKey);
 
   hmac.update(content);
   return hmac.digest('hex');
--- api/src/crypto/hash.test.js
+++ api/src/crypto/hash.test.js
@@ -28,46 +28,77 @@ describe('Hash', () => {
     crypto.createHmac.mockReturnValue(hmac);
 
     hash = createHash(crypto);
-    result = hash('foobar');
   });
 
-  it('should create key of 64 random bytes', () => {
-    expect(crypto.randomBytes).toHaveBeenCalledWith(64);
-  });
-
-  it('should get key as base64', () => {
-    expect(randomBytes.toString).toHaveBeenCalledWith('base64');
-  });
-
-  it('should create hmac-sha256 with the created key', () => {
-    expect(crypto.createHmac).toHaveBeenCalledWith('sha256', 'key');
-  });
-
-  it('should update the given content to digest', () => {
-    expect(hmac.update).toHaveBeenCalledWith('foobar');
-  });
-
-  it('should produce the digest as hex', () => {
-    expect(hmac.digest).toHaveBeenCalledWith('hex');
-  });
-
-  it('should return the digest result', () => {
-    expect(result).toEqual('hash');
-  });
-
-  it('should result in 64-character digest', () => {
-    const actualHash = createHash();
-    const actualResult = actualHash('foo');
-    expect(actualResult.length).toBe(64);
-  });
-
-  it('should not produce same result for same hashed content', () => {
-    const actualHash = createHash();
-    const results = Array(100).fill().map(() => actualHash('foo'));
-
-    results.forEach((item) => {
-      const resultsWithoutItem = results.filter(element => element !== item);
-      expect(resultsWithoutItem).not.toContain(item);
+  function testHash() {
+    it('should update the given content to digest', () => {
+      expect(hmac.update).toHaveBeenCalledWith('foobar');
     });
+
+    it('should produce the digest as hex', () => {
+      expect(hmac.digest).toHaveBeenCalledWith('hex');
+    });
+
+    it('should return the digest result', () => {
+      expect(result).toEqual('hash');
+    });
+
+    it('should result in 64-character digest', () => {
+      const actualHash = createHash();
+      const actualResult = actualHash('foo');
+      expect(actualResult.length).toBe(64);
+    });
+  }
+
+  describe('when key is not given', () => {
+    beforeEach(() => {
+      result = hash('foobar');
+    });
+
+    it('should create key of 64 random bytes', () => {
+      expect(crypto.randomBytes).toHaveBeenCalledWith(64);
+    });
+
+    it('should get key as base64', () => {
+      expect(randomBytes.toString).toHaveBeenCalledWith('base64');
+    });
+
+    it('should create hmac-sha256 with the created key', () => {
+      expect(crypto.createHmac).toHaveBeenCalledWith('sha256', 'key');
+    });
+
+    it('should not produce same result for same hashed content', () => {
+      const actualHash = createHash();
+      const results = Array(100).fill().map(() => actualHash('foo'));
+
+      results.forEach((item) => {
+        const resultsWithoutItem = results.filter(element => element !== item);
+        expect(resultsWithoutItem).not.toContain(item);
+      });
+    });
+
+    testHash();
+  });
+
+  describe('when key is given', () => {
+    beforeEach(() => {
+      result = hash('foobar', 'foobarkey');
+    });
+
+    it('should not create key', () => {
+      expect(crypto.randomBytes).not.toHaveBeenCalled();
+    });
+
+    it('should create hmac-sha256 with the given key', () => {
+      expect(crypto.createHmac).toHaveBeenCalledWith('sha256', 'foobarkey');
+    });
+
+    it('should produce the same result for the same hashed content', () => {
+      const actualHash = createHash();
+      const results = [actualHash('foo', 'bar'), actualHash('foo', 'bar'), actualHash('foo', 'bar')];
+      expect(results.every(value => value === results[0])).toEqual(true);
+    });
+
+    testHash();
   });
 });
